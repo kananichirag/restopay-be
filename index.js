4bd0e5a1101aa7dadd9364ff8ed68a688e3505f9
kananichirag <chiragkanani144@gmail.com>
2025-01-19 21:23:08 +0530

    added socket for order

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
 const IndexRoutes = require("./routes/IndexRoutes");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const setupOrderSocket = require("./sockets/orderSocket");
+
 dotenv.config();
 const PORT = process.env.PORT || 3030;
-const HOST = '192.168.1.3';
-
+const HOST = '192.168.1.2';
 
 app.use(cors());
 app.use(express.json());
 app.use("/v1", IndexRoutes);
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", "http://192.168.1.2:5173"], 
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+});
+
+setupOrderSocket(io);
+
 mongoose
   .connect(process.env.MONGO_URL)
-  .then((e) => console.log("MongoDB Connected .!!"))
+  .then(() => console.log("MongoDB Connected .!!"))
   .catch((err) => console.log("Error to Connecting MongoDB", err));
-app.listen(PORT, () => console.log("Server Start"));
+
+server.listen(PORT, HOST, () => console.log(`Server running at http://${HOST}:${PORT}`));
