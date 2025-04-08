8897351ed55b354456828694699080f72eb5b66c
chiragkanani <chiragkanani144@gmail.com>
2025-04-08 18:57:47 +0530

    change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/v1", IndexRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://192.168.195.37:5173", "https://resto-pay.netlify.app/"],
+    origin: ["http://localhost:5173", "http://192.168.195.37:5173", "https://resto-pay.netlify.app"],
     methods: ["GET", "POST"],
     credentials: true
   },
